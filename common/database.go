9849f7d5df9410aad5c1cafa3196869f388c6677
Antanidoss <antosci2002@gmail.com>
2023-02-19 13:34:08 +0300

    Added check Db created

--- common/database.go
+++ common/database.go
@@ -21,26 +21,50 @@ func Init() *sql.DB {
 		panic("Error loading credentials: %v" + envErr.Error())
 	}
 
-	server := os.Getenv("MSSQL_DB_SERVER")
-	sqlServerConnectionString := fmt.Sprintf("odbc:server=%s", server)
+	var (
+		server = os.Getenv("MSSQL_DB_SERVER")
+		dbName = os.Getenv("MSSQL_DB_NAME")
+	)
 
+	dbConnectionString := fmt.Sprintf("odbc:server=%s;database=%s", server, dbName)
+	db, isCreated := checkDbCreated(dbConnectionString)
+
+	if isCreated {
+		DB = db
+		return DB
+	}
+
+	sqlServerConnectionString := fmt.Sprintf("odbc:server=%s", server)
 	db, err := sql.Open(providerName, sqlServerConnectionString)
 
 	if err != nil {
-		panic("db err: (Init) " + err.Error())
+		panic("db err: (Init) %v" + err.Error())
 	}
 
 	createDb(db)
-
-	dbName := os.Getenv("MSSQL_DB_NAME")
-	dbConnectionString := fmt.Sprintf("odbc:server=%s;database=%s", server, dbName)
 	DB, err := sql.Open(providerName, dbConnectionString)
 
 	if err != nil {
-		panic("db err: (Init) " + err.Error())
+		panic("db err: (Init) %v" + err.Error())
 	}
 
 	return DB
+}
+
+func checkDbCreated(dbConnectionString string) (*sql.DB, bool) {
+	db, err := sql.Open(providerName, dbConnectionString)
+
+	if err != nil {
+		panic("db err: (Init) %v" + err.Error())
+	}
+
+	err = db.Ping()
+
+	if err == nil {
+		return db, true
+	}
+
+	return nil, false
 }
 
 func createDb(DB *sql.DB) {
