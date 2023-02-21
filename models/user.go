cbc286594265428032ed4641e0632050ebfe0700
Antanidoss <antosci2002@gmail.com>
2023-02-21 22:42:54 +0300

    Configured server with gin

--- models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type user struct {
-	ID           uint
-	Username     string
-	Email        string
-	PasswordHash string
-	TodoItems    []todoItem
-}
---
+++ todoItem.go
@@ -0,0 +1,21 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type todoItem struct {
+	ID          uint
+	name        string
+	description string
+}
+
+func addTodoitem(ctx *gin.Context) {
+
+}
+
+func updateTodoItem(ctx *gin.Context) {
+
+}
+
+func getTodoitemById(ctx *gin.Context) {
+
+}
